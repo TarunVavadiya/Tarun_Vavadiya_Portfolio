90daa46ac5fc2cf12056e750e9a826882113e968
Tarun Vavadiya <tarun.v@lanatussystems.com>
2023-11-05 19:44:55 +0530

    update resume

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import tarunResume from "../../assets/Tarun_Vavadiya.pdf";
 // import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 // import { CgGitFork } from "react-icons/cg";
@@ -37,7 +38,9 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <h1 style={{ marginTop: "3%" }} className="yellow">Tarun Vavadiya</h1>
+          <h1 style={{ marginTop: "3%" }} className="yellow">
+            Tarun Vavadiya
+          </h1>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
@@ -83,20 +86,28 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/resume"
+                to="/contact"
                 onClick={() => updateExpanded(false)}
               >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+                <AiOutlineContacts style={{ marginBottom: "2px" }} /> Contact Me
               </Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/contact"
-                onClick={() => updateExpanded(false)}
+                onClick={() => {
+                  updateExpanded(false);
+                  const fileName = "Tarun-Vavadiya-CV.pdf";
+                  const fileUrl = tarunResume;
+
+                  const link = document.createElement("a");
+                  link.href = fileUrl;
+                  link.download = fileName;
+                  link.click();
+                }}
               >
-                <AiOutlineContacts style={{ marginBottom: "2px" }} /> Contact Me
+                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
             </Nav.Item>
 
